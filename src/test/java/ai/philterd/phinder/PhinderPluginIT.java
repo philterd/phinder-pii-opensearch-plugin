51c5a60c29a88dbd35afbdca68e05f87abbdcbcf
jzonthemtn <jeff.zemerick@philterd.ai>
2024-11-14 20:48:21 -0500

    Working on dependencies. Setting Opensearch to 2.18.0.

--- src/test/java/ai/philterd/phinder/PhinderPluginIT.java
+++ src/test/java/ai/philterd/phinder/PhinderPluginIT.java
@@ -8,19 +8,16 @@
 package ai.philterd.phinder;
 
 import com.carrotsearch.randomizedtesting.annotations.ThreadLeakScope;
-import org.apache.hc.core5.http.ParseException;
-import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.http.util.EntityUtils;
+import org.junit.Assert;
 import org.opensearch.client.Request;
 import org.opensearch.client.Response;
 import org.opensearch.plugins.Plugin;
 import org.opensearch.test.OpenSearchIntegTestCase;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.Collection;
 import java.util.Collections;
-
-import static org.hamcrest.Matchers.containsString;
 
 @ThreadLeakScope(ThreadLeakScope.Scope.NONE)
 @OpenSearchIntegTestCase.ClusterScope(scope = OpenSearchIntegTestCase.Scope.SUITE)
@@ -31,11 +28,11 @@ public class PhinderPluginIT extends OpenSearchIntegTestCase {
         return Collections.singletonList(PhinderPlugin.class);
     }
 
-    public void testPluginInstalled() throws IOException, ParseException {
+    public void testPluginInstalled() throws IOException {
         Response response = getRestClient().performRequest(new Request("GET", "/_cat/plugins"));
-        String body = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
+        String body = EntityUtils.toString(response.getEntity());
 
         logger.info("response body: {}", body);
-        assertThat(body, containsString("phinder"));
+        Assert.assertTrue(body.contains("phinder"));
     }
 }
